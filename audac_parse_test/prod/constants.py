463ffc500dc24b1fff24fb634d6a2992cf7cacf7
vtslv <vstolopov@yandex.ru>
2024-03-10 00:21:09 +0300

    upd output, create temp_file and unique_temp_file, logic for replace, need replace data dict

--- audac_parse_test/prod/constants.py
+++ audac_parse_test/prod/constants.py
@@ -4,3 +4,13 @@ PRODUCT_URLS = [
     'https://audac.eu/eu/products/d/alti6---2-way-6inch-pendant-speaker#section-specifications',
     'https://audac.eu/eu/products/d/xeno6---full-range-speaker-6inch#section-specifications',
 ]
+
+
+keyword_dictionary = {
+    'Тип АС' : ['Speaker type'],
+    'Широкополосная' : ['Full range'],
+    'Пиковая мощность' : ['Peak power handling'],
+    'Продолжительная мощность' : ['RMS/AES power handling'],
+    'Отклик (± 3 дБ)' : ['Response (± 3 dB)'],
+    '208 Ω - 100 В / Нет - 70 В / 24 Вт' : ['208 Ω - 100 V / Not used - 70 V / 24 W'],
+}
